191b5a134f346c5c7b06112506385bbe786e9375
Rikk <tarikmoussa90@gmail.com>
2026-06-19 16:08:29 -0300

    feat: segmento da página 27 de introdução a type/nodejs

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,14 +1,14 @@
 import './App.css'
 
-export default function App(){
-
-  return(
-    <>
-    <h1>TDS FIAP - RwD</h1>
-    <a href="#"> FIAP </a>
-    <br />
-    <a href="#">GOOGLE</a>
-    </>
-  )
-
-}
+//export default function App(){
+//
+//  return(
+//   <>
+//    <h1>TDS FIAP - RwD</h1>
+//    <a href="#"> FIAP </a>
+//    <br />
+//    <a href="#">GOOGLE</a>
+//    </>
+//  )
+//
+//}
