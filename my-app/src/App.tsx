2579f46492a7c5d77b738a336f73c9761aa4c89d
Rikk <tarikmoussa90@gmail.com>
2026-06-19 16:04:50 -0300

    feat: hiperlinks para o localhost com app.tsx

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,10 +1,14 @@
-
 import './App.css'
 
-function App() {
-  return (
-    <h1>Olá Mundo!!</h1>
-  )
-}
+export default function App(){
 
-export default App
+  return(
+    <>
+    <h1>TDS FIAP - RwD</h1>
+    <a href="#"> FIAP </a>
+    <br />
+    <a href="#">GOOGLE</a>
+    </>
+  )
+
+}
