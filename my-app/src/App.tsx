842633047c2b1c4afa8d30149d9511eaac0e4a1b
Rikk <tarikmoussa90@gmail.com>
2026-06-20 15:45:23 -0300

    fix: bug do png/jpg corrigido e logo com px responsivo à tela

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 import Lista from './componentes/ListaAluno'
-import Fiap1 from './assets/fiap1.jpg'
+import Fiap1 from './assets/fiap1.png'
 
 export default function App(){
   const aluno = "Tárik M. Alma "
@@ -9,6 +9,7 @@ export default function App(){
   return(
     <>
     <h1>TDS FIAP - RwD</h1>
+    <img src={Fiap1} width='100px' />
     <p>Alunos: {aluno} </p>
     <p>Curso: {curso} </p>
     <Lista/>
