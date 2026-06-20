1c280e7eda3ac137147563a6ed38917db93d8bdb
Rikk <tarikmoussa90@gmail.com>
2026-06-20 16:13:29 -0300

    feat: exercicio intro.react pg32

---
+++ exercicio/src/componentes/Cabecalho.tsx
@@ -0,0 +1,7 @@
+export default function Cabecalho() {
+    return (
+        <header>
+            <h1>Lista de Alunos</h1>
+        </header>
+    );
+}
--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,18 +1,5 @@
 import './App.css'
-import Lista from './componentes/ListaAluno'
-import Fiap1 from './assets/fiap1.png'
 
 export default function App(){
-  const aluno = "Tárik M. Alma "
-  const curso = 'ADS'
-
-  return(
-    <>
-    <h1>TDS FIAP - RwD</h1>
-    <img src={Fiap1} width='100px' />
-    <p>Alunos: {aluno} </p>
-    <p>Curso: {curso} </p>
-    <Lista/>
-    </>
-  )
+    
 }
